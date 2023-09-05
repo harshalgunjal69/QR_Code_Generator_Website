a73049e81b3c13e3bd43b8ad867dd3ff73f1ccf1
harshalgunjal69 <hrshlgunjal@gmail.com>
2023-09-05 14:27:49 +0530

    initial commit

--- app.py
+++ app.py
@@ -16,10 +16,18 @@ def index():
 def generateQR():
     memory=BytesIO()
     data = request.form.get('dataQR')
-    img = qrcode.make(data)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=7,
+        border=2)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
     img.save(memory)
     memory.seek(0)
     base64_img = "data:image/png;base64," + base64.b64encode(memory.getvalue()).decode('ascii')
+
     return render_template('index.html', data=base64_img)
     
 
