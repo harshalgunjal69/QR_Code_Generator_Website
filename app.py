2d0cbe908e5089c9a6b45084811f68e9a001ee97
harshalgunjal69 <hrshlgunjal@gmail.com>
2023-09-05 13:48:04 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #importing the modules
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import qrcode
+from io import BytesIO
+import base64
 
 #initializing the flask app
 app = Flask(__name__)
@@ -10,7 +12,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def generateQR():
+    memory=BytesIO()
+    data = request.form.get('dataQR')
+    img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+    base64_img = "data:image/png;base64," + base64.b64encode(memory.getvalue()).decode('ascii')
+    return render_template('index.html', data=base64_img)
+    
+
 # main driver function
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
